29b5dc1cd9a80ad836b410cea8d7d24ccab0a56c
Mohan <seethamohansai@icloud.com>
2024-06-11 17:50:20 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// JavaScript functionality can be added here if needed
+document.addEventListener("DOMContentLoaded", () => {
+  // Example: Add event listener to banner button
+  document.querySelector(".banner button").addEventListener("click", () => {
+    alert("Shop Now button clicked!");
+  });
+});
